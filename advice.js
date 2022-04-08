e31b320afad51e824fbe7b990e23a2758109b246
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-04-08 09:00:48 +0100

    Handled error properly

--- advice.js
+++ advice.js
@@ -2,15 +2,25 @@ const dice = document.getElementById("dice");
 const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 
-dice.addEventListener("click", function () {
+dice.addEventListener("click", displayAdvice);
+
+function displayAdvice() {
   async function getAdvice() {
-    const adviceObject = await fetch("https://api.adviceslip.com/advice");
-    const realAdvice = await adviceObject.json();
-    const { advice, id } = realAdvice.slip;
-    adviceText.classList.add("animate");
-    adviceText.innerText = `"${advice}"`;
-    adviceId.innerText = `#${id}`;
+    try {
+      const adviceObject = await fetch("https://api.adviceslip.com/advice");
+      if (adviceObject.ok) {
+        const realAdvice = await adviceObject.json();
+        const { advice, id } = realAdvice.slip;
+        adviceText.classList.add("animate");
+        adviceText.innerText = `"${advice}"`;
+        adviceId.innerText = `#${id}`;
+      } else {
+        throw new Error("Failed to fetch advice 😔");
+      }
+    } catch (error) {
+      adviceText.innerText = "Failed to fetch advice 😔";
+    }
   }
   getAdvice();
   adviceText.classList.remove("animate");
-});
+}
